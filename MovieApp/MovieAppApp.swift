6d534900f73385308592dc83ce08aaf8853ca078
ismailhamad <ismailhamad2001@gmail.com>
2022-08-08 15:22:09 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ismail on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
